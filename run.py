e35fe893641aaf3e6e66881dbb899fe9b167d585
cefeidas <danmarmon1986@gmail.com>
2023-04-09 19:20:10 +0000

    getting the function to update the worksheet

--- run.py
+++ run.py
@@ -71,6 +71,15 @@ def turn(player):
     else:
         print(f"{dice_choice} is not a valid sum of two numbers in the list {numbers}. Please, try again")
 
+def update_worksheet(data,worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet 
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
 
 turn(P1)
 
