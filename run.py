d40f9831c1b5e4f499a49884745fe8dc0e7b4999
cefeidas <danmarmon1986@gmail.com>
2023-04-15 22:55:14 +0000

    Separated the funtion turn into first_turn() and following_turns()

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ def naming_the_players():
     P2 = input('Player two, please enter your name. ')
     return [P1, P2]
 
-def turn(player):
+def first_turn(player):
     while True:
         numbers = [random.randint(1, 6) for i in range(4)]
         print(f"{player}, your first roll is: {numbers}")
@@ -58,11 +58,46 @@ def turn(player):
                 print(f"{dice_choice} is a valid combination of two numbers in the list {numbers}.")
                 break
 
-        target_number = dice_choice
+        target_number = {dice_choice: 1}
+        break
+    result = [target_number]
+    print(f'You chose the number {list(target_number.keys())[0]}. You advanced 1 cell.')
+    return result
+
+
+"""
+def following_turns(player):
+    while True:
+        numbers = [random.randint(1, 6) for i in range(4)]
+        print(f"{player}, your first roll is: {numbers}")
+        while True:
+            try:
+                dice_choice = int(input("From those four numbers, please choose one combination of two dice summed: "))
+            except ValueError as e:
+                print(f"Invalid data: {e}, please try again.\n")
+                continue
+
+            valid_combination = False
+            all_combinations = list(itertools.combinations(numbers, 2))
+            for comb in all_combinations:
+                if dice_choice == sum(comb):
+                    valid_combination = True
+                    break
+
+            if not valid_combination:
+                print(f"{dice_choice} is not a valid combination of two numbers in the list {numbers}. Please try again.")
+            else:
+                print(f"{dice_choice} is a valid combination of two numbers in the list {numbers}.")
+                break
+
+        target_number = first_turn(Player)
+        if dice_choice == [target_number]:
+        
         break
     result = [target_number]
     print(result)
     return result
+"""
 
 
-turn('Pepito')
+first_turn('Pepito')
