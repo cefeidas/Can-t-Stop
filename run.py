6524cf9b38e312b01189e789e9b39cbe424b9d03
cefeidas <danmarmon1986@gmail.com>
2023-04-16 22:05:16 +0000

    Simplifying turn() function by splitting it into smaller functions

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def presenting_the_game():
     This function prints introductory sentences to help the players understand the game before it starts.
     """
     print("Welcome to Can't stop, a push your luck game.")
-    print('This is a simplified version of the game. If you are interestedin the original rules, please visit: "https://www.ultraboardgames.com/cant-stop/game-rules.php"')
+    print('This is a simplified version of the game. If you are interested in the original rules, please visit: "https://www.ultraboardgames.com/cant-stop/game-rules.php"')
     print('The rules for this project can be found in the README file.')
     print("Let's get started!")
 
@@ -37,6 +37,62 @@ def naming_the_players():
     P2 = input('Player Two, please enter your name (choose a name that is different from Player One): ')
     return [P1, P2]
 
+def roll_dice():
+    """
+    This function rolls the four dice and returns the resulting numbers.
+    """
+    return [random.randint(1, 6) for i in range(4)]
+
+
+def get_dice_combinations(numbers):
+    """
+    This function returns all the possible combinations of two dice from the rolled numbers.
+    """
+    return list(itertools.combinations(numbers, 2))
+
+
+def get_valid_choice(target_number, dice_combinations):
+    """
+    This function validates the player's choice of dice numbers and returns the chosen number if it's valid.
+    """
+    while True:
+        try:
+            dice_choice = int(input("From those four numbers, choose any two numbers and add them together, or enter your target number if you already have one: "))
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+            continue
+
+        valid_combination = False
+        for comb in dice_combinations:
+            if dice_choice == sum(comb):
+                valid_combination = True
+                break
+
+        if target_number and target_number[0] not in [sum(comb) for comb in dice_combinations]:
+            print("It seems you ran out of luck")
+            print(f"You pushed your luck too hard and will go back to the starting square for this turn: ({target_number})")
+            return None
+        elif not valid_combination:
+            print(f"{dice_choice} is not the result of adding any two of the rolled dice. Please try again.")
+            continue
+        else:
+            return dice_choice
+
+
+def should_continue_rolling():
+    """
+    This function asks the player if they want to continue rolling the dice and returns True if they do, False otherwise.
+    """
+    while True:
+        continue_rolling = input("Do you want to continue rolling the dice? Y/N: ").upper()
+        if continue_rolling == 'Y':
+            return True
+        elif continue_rolling == 'N':
+            return False
+        else:
+            print("Invalid input. Please enter Y or N.")
+
+
 def turn(target_number, player):
     """
     This function handles all actions that occur in each turn of the game.
@@ -45,53 +101,25 @@ def turn(target_number, player):
     scored = False
 
     while True:
-        numbers = [random.randint(1, 6) for i in range(4)]
+        numbers = roll_dice()
         print(f"{player}, you rolled the following numbers: {numbers}")
-        while True:
-            try:
-                dice_choice = int(input("From those four numbers, choose any two numbers and add them together, or enter your target number if you already have one: "))
-            except ValueError as e:
-                print(f"Invalid data: {e}, please try again.\n")
-                continue
+        dice_combinations = get_dice_combinations(numbers)
+        dice_choice = get_valid_choice(target_number, dice_combinations)
 
-            valid_combination = False
-            all_combinations = list(itertools.combinations(numbers, 2))
-            for comb in all_combinations:
-                if dice_choice == sum(comb):
-                    valid_combination = True
-                    break
+        if dice_choice is None:
+            return original_target_number, False
+        elif not target_number:
+            target_number = [dice_choice, 1]
+            scored = True
+        elif dice_choice == target_number[0]:
+            target_number[1] += 1
+            scored = True
 
-            if target_number and target_number[0] not in [sum(comb) for comb in all_combinations]:
-                print("It seems you ran out of luck")
-                print(f"You pushed your luck too hard and will go back to the starting square for this turn: ({original_target_number})")
-                return original_target_number, False
-            elif not valid_combination:
-                print(f"{dice_choice} is not the result of adding any two of the rolled dice({numbers}). Please try again.")
-                continue
-            elif not target_number:
-                target_number = [dice_choice, 1]
-                scored = True
-                break
-            elif dice_choice == target_number[0]:
-                target_number[1] += 1
-                scored = True
-                break
-            else:
-                print("This is not your target number.")
-                continue
+        print(f'You chose the number {target_number[0]}. You moved up to the row {target_number[1]}.')
 
-        result = target_number
-        print(f'You chose the number {result[0]}. You moved up to the row {result[1]}.')
-
-        while True:
-            continue_rolling = input("Do you want to continue rolling the dice? Y/N: ").upper()
-            if continue_rolling == 'Y':
-                break
-            elif continue_rolling == 'N':
-                print(f"The result is: {result}")
-                return result, scored
-            else:
-                print("Invalid input. Please enter Y or N.")
+        if not should_continue_rolling():
+            print(f"The result is: {target_number}")
+            return target_number, scored
 
 def update_sheet(coordinates, player, scored):
     """
